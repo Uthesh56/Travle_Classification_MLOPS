cbc8a707e1d15b2ae75558b9988a279a982ab947
Uthesh <utheshganapathy@gmail.com>
2025-01-09 19:52:37 +0530

    Updated

---
+++ Setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="Travel_Classification",
+    version="0.0.0",
+    author="Uthesh",
+    author_email="utheshganapathy@gmail.com",
+    packages=find_packages()
+)
